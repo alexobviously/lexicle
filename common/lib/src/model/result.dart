1c0d80a25f3268b3aa0e337a26d6ada07f030e9e
Alex Baker <iamalexbaker@gmail.com>
2022-01-23 20:15:17 +0700

    Result object

---
+++ common/lib/src/model/result.dart
@@ -0,0 +1,16 @@
+/// A generic structure for results to be returned in.
+class Result<T> {
+  final T? object;
+  final String? error;
+  final List<String> warnings;
+
+  bool get ok => error == null;
+  bool get hasObject => object != null;
+
+  Result({this.object, this.error, this.warnings = const []}) {
+    assert(!(error == null && object == null));
+  }
+
+  factory Result.error(String error, [List<String> warnings = const []]) => Result(error: error, warnings: warnings);
+  factory Result.ok(T object, [List<String> warnings = const []]) => Result(object: object, warnings: warnings);
+}
