7b7ed8a6dc86766267ba92d23a0f5388e562f7d7
TheBeville <steve_beville@hotmail.com>
2022-01-23 16:07:17 +0000

    added game end widget

---
+++ app/lib/game_end.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'app/colours.dart';
+
+class GameEnd extends StatelessWidget {
+  final int guesses;
+
+  const GameEnd({
+    Key? key,
+    required this.guesses,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return AspectRatio(
+      aspectRatio: 620 / 261,
+      child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(width: 1, color: Colours.wrong),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('Congratulations!', style: textTheme.headline4),
+              Text(
+                'You figured out the word in $guesses guesses',
+                style: textTheme.headline5,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          )),
+    );
+  }
+}
