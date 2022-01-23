858929794870a8921c2a1bdb67c3601303b38483
Alex Baker <iamalexbaker@gmail.com>
2022-01-23 18:40:13 +0700

    Removed model from server

--- server/bin/model/game.dart
+++
@@ -1,8 +0,0 @@
-class Game {
-  final String? id;
-  final String answer;
-  final String player;
-  final String? creator;
-
-  Game({this.id, required this.answer, required this.player, this.creator});
-}
