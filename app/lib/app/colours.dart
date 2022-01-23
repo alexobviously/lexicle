4a2c846b6f019c54cc94d55990443be972163d57
Alex Baker <iamalexbaker@gmail.com>
2022-01-23 14:07:18 +0700

    Colour scheme update

--- app/lib/app/colours.dart
+++ app/lib/app/colours.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 
+// original
+// class Colours {
+//   static const Color correct = Color(0xFFC8E6C9);
+//   static const Color semiCorrect = Color(0xFFFFECB3);
+//   static const Color wrong = Color(0xFFBDBDBD);
+//   static const Color invalid = Colors.red;
+// }
+
+// bright
+// class Colours {
+//   static const Color correct = Color(0xFFA1EF8B);
+//   static const Color semiCorrect = Color(0xFFFFBF00);
+//   static const Color wrong = Color(0xFFB2B4BD);
+//   static const Color invalid = Color(0xFFBA1200);
+// }
+
+// compromise
 class Colours {
-  static Color correct = Colors.green.shade100;
-  static Color semiCorrect = Colors.amber.shade100;
-  static Color wrong = Colors.grey.shade400;
-  static Color invalid = Colors.red;
+  static const Color correct = Color(0xFFC3EDC0);
+  static const Color semiCorrect = Color(0xFFF9E08A);
+  static const Color wrong = Color(0xFFC8CAD0);
+  static const Color invalid = Color(0xFFD8464B);
+  static const Color victory = Color(0xFFA1E1F7);
 }
