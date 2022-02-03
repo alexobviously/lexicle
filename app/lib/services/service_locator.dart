625ba381c1483471e33e992842de70c09c01c7fe
Alex Baker <iamalexbaker@gmail.com>
2022-02-03 23:43:53 +0700

    Added secure storage

--- app/lib/services/service_locator.dart
+++ app/lib/services/service_locator.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:word_game/cubits/auth_controller.dart';
 import 'package:word_game/services/app_dictionary.dart';
@@ -6,8 +7,12 @@ GetIt getIt = GetIt.I;
 
 AppDictionary dictionary() => getIt.get<AppDictionary>();
 AuthController auth() => getIt.get<AuthController>();
+FlutterSecureStorage storage() => getIt.get<FlutterSecureStorage>();
 
 Future<void> setUpServiceLocator() async {
   getIt.registerSingleton<AppDictionary>(AppDictionary());
   getIt.registerSingleton<AuthController>(AuthController());
+  getIt.registerSingleton<FlutterSecureStorage>(FlutterSecureStorage(
+    aOptions: AndroidOptions(encryptedSharedPreferences: true),
+  ));
 }
