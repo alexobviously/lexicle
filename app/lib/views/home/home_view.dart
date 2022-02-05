fa5b1bccfabdbe67234e519e5dd265ca66331e0f
Alex Baker <iamalexbaker@gmail.com>
2022-02-05 12:27:53 +0700

    HomeView refactor

--- app/lib/views/home/home_view.dart
+++ app/lib/views/home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:word_game/app/colours.dart';
 import 'package:word_game/app/routes.dart';
 import 'package:word_game/cubits/auth_controller.dart';
 import 'package:word_game/ui/standard_scaffold.dart';
@@ -22,63 +23,56 @@ class _HomeViewState extends State<HomeView> {
       showBackButton: false,
       body: Center(
         child: SafeArea(
-          child: Column(
-            children: [
-              Text(
-                'Lexicle',
-                style: textTheme.headline3,
-                textAlign: TextAlign.center,
-              ),
-              Container(height: 50),
-              BlocBuilder<AuthController, AuthState>(
-                builder: (context, state) {
-                  if (state.loggedIn) {
-                    return UserDetails(user: state.user!);
-                  } else {
-                    return Container();
-                  }
-                },
-              ),
-              Container(height: 20),
-              NeumorphicButton(
-                onPressed: () => Navigator.of(context).pushNamed(Routes.solo),
-                style: NeumorphicStyle(
-                  shape: NeumorphicShape.flat,
-                  boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(16)),
+          child: BlocBuilder<AuthController, AuthState>(builder: (context, state) {
+            return Column(
+              children: [
+                Text(
+                  'Lexicle',
+                  style: textTheme.headline3,
+                  textAlign: TextAlign.center,
                 ),
-                child: Text('Play Offline', style: textTheme.headline6),
-              ),
-              Container(height: 20),
-              NeumorphicButton(
-                onPressed: () => Navigator.of(context).pushNamed(Routes.groups),
-                style: NeumorphicStyle(
-                  shape: NeumorphicShape.flat,
-                  boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(16)),
+                Container(height: 30),
+                BlocBuilder<AuthController, AuthState>(
+                  builder: (context, state) {
+                    if (state.loggedIn) {
+                      return UserDetails(user: state.user!);
+                    } else {
+                      return Container();
+                    }
+                  },
                 ),
-                child: Text('Play Online', style: textTheme.headline6),
-              ),
-              Container(height: 20),
-              NeumorphicButton(
-                onPressed: () => Navigator.of(context).pushNamed(Routes.dict),
-                style: NeumorphicStyle(
-                  shape: NeumorphicShape.flat,
-                  boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(16)),
+                Container(height: 20),
+                NeumorphicButton(
+                  onPressed: () => Navigator.of(context).pushNamed(Routes.solo),
+                  style: NeumorphicStyle(
+                    shape: NeumorphicShape.flat,
+                    boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(16)),
+                  ),
+                  child: Text('Play Offline', style: textTheme.headline6),
                 ),
-                child: Text('Dictionary', style: textTheme.headline6),
-              ),
-              Container(height: 20),
-              NeumorphicButton(
-                onPressed: () => Navigator.of(context).pushNamed(Routes.auth),
-                style: NeumorphicStyle(
-                  shape: NeumorphicShape.flat,
-                  boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(16)),
+                Container(height: 20),
+                NeumorphicButton(
+                  onPressed: () => Navigator.of(context).pushNamed(state.loggedIn ? Routes.groups : Routes.auth),
+                  style: NeumorphicStyle(
+                    shape: NeumorphicShape.flat,
+                    boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(16)),
+                  ),
+                  child: Text(state.loggedIn ? 'Play Online' : 'Login', style: textTheme.headline6),
                 ),
-                child: Text('Login', style: textTheme.headline6),
-              ),
-              Spacer(),
-              _version(),
-            ],
-          ),
+                Container(height: 20),
+                NeumorphicButton(
+                  onPressed: () => Navigator.of(context).pushNamed(Routes.dict),
+                  style: NeumorphicStyle(
+                    shape: NeumorphicShape.flat,
+                    boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(16)),
+                  ),
+                  child: Text('Dictionary', style: textTheme.headline6),
+                ),
+                Spacer(),
+                _version(),
+              ],
+            );
+          }),
         ),
       ),
     );
