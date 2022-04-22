95dc92b8bc2b8ac897e826558e938af0affd1e4f
Alex Baker <iamalexbaker@gmail.com>
2022-04-23 01:15:00 +0700

    Update challenge_store.dart

--- common/lib/src/stores/challenge_store.dart
+++ common/lib/src/stores/challenge_store.dart
@@ -1,21 +1,25 @@
 import 'package:common/common.dart';
 
 class ChallengeStore extends EntityStore<Challenge> {
-  final Dictionary dictionary;
+  final Dictionary? dictionary;
   final int? key;
   bool get isAuthority => key != null;
 
   ChallengeStore(
     DatabaseService db, {
-    required this.dictionary,
+    this.dictionary,
     this.key,
-  }) : super(db);
+  })  : assert(key == null || dictionary != null, 'Dictionary must be provided if key is provided'),
+        super(db);
 
   Future<Result<Challenge>> getBySequence(int level, int sequence) async {
     List<Challenge> matches = items.values.where((e) => e.level == level && e.sequence == sequence).toList();
     if (matches.isNotEmpty) return Result.ok(matches.first);
     final c = await db.getChallenge(level, sequence);
-    if (c.ok) return Result.ok(c.object!);
+    if (c.ok) {
+      onGet(c.object!);
+      return Result.ok(c.object!);
+    }
     return Result.error(c.error!);
   }
 
@@ -29,7 +33,10 @@ class ChallengeStore extends EntityStore<Challenge> {
     }
 
     final c = await db.getCurrentChallenge(level);
-    if (c.ok) return Result.ok(c.object!);
+    if (c.ok) {
+      onGet(c.object!);
+      return Result.ok(c.object!);
+    }
     if (isAuthority) return Result.ok(create(level, sequence));
     return Result.error(Errors.notFound);
   }
@@ -37,7 +44,7 @@ class ChallengeStore extends EntityStore<Challenge> {
   Challenge create(int level, int? sequence) {
     int _today = today().millisecondsSinceEpoch;
     final config = Challenges.config(level);
-    String word = dictionary.randomWord(config.wordLength, seed: _today % (key ?? defaultChallengeKey));
+    String word = dictionary!.randomWord(config.wordLength, seed: _today % (key ?? defaultChallengeKey));
     Challenge c = Challenge(
       level: level,
       sequence: sequence,
